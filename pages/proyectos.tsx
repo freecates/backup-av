8fcb4f5e826a5cd8edd134c422b3d06bd773470a
Ramon Gil <ramon.gil@deporvillage.com>
2021-10-31 17:51:00 +0100

    with-projects, added project page and making the layout

---
+++ pages/proyectos.tsx
@@ -0,0 +1,71 @@
+import Layout from '@components/layout';
+import api from '@libs/api.js';
+import styles from '@styles/Page.module.scss';
+import Button from '@components/button';
+import { GetStaticProps, NextPage } from 'next';
+import { IRoute } from '@interfaces/index';
+
+interface Props {
+    routes: IRoute[];
+}
+
+const Clientes: NextPage<Props> = ({ routes }) => {
+    return (
+        <Layout
+            pageTitle={'Proyectos'}
+            pageDescription={'Proyectos Backup AV'}
+            siteTitle={'Proyectos Backup AV'}
+            navRoutes={routes}
+        >
+            <div className={styles['with-portfolio']}>
+                <div className={`${styles.container}`}>
+                    <main className={styles.main}>
+                        <h1 className={styles.title}>Vuestros proyectos son nuestro hogar</h1>
+                        <p className={styles.description}>
+                            Aquí hay algunos que nos encantaría compartir.
+                        </p>
+                        <div className={styles['button-wrapper']}>
+                            <Button name={'Contacta'} isAnchor url={'/contacta'} />
+                        </div>
+                    </main>
+                </div>
+            </div>
+            <div className={styles['bg-projects']}>
+                <div className={styles['portfolio-container']}>
+                    <div
+                        className={styles['featured-project']}
+                        style={{
+                            backgroundImage:
+                                'url(https://images.prismic.io/monogram/9398188e-b087-4d64-807f-d5b23fd415d3_m-bg.jpg?auto=compress,format)',
+                            backgroundColor: '#764d9b',
+                        }}
+                    >
+                        <div className={styles['logo-project']}>
+                            <img
+                                src='https://images.prismic.io/monogram/a769cac1-e373-4313-871a-f4f72dd9b7d8_m-logo.svg?auto=compress,format'
+                                alt=''
+                                width='232'
+                                height='167'
+                                className='flex-1'
+                                style={{ maxWidth: '250px', maxHeight: '125px' }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+    const [routes] = await Promise.all([api.routes.getData()]);
+
+    return {
+        props: {
+            routes,
+        },
+        revalidate: 1,
+    };
+};
+
+export default Clientes;
