33638de094c76329ddf5275324d4a9ea132e2310
Ramon Gil <ramon.gil@deporvillage.com>
2021-11-01 14:33:28 +0100

    with-projects, added layout scss for projects section

--- pages/proyectos.tsx
+++ pages/proyectos.tsx
@@ -1,4 +1,5 @@
 import Layout from '@components/layout';
+import Image from 'next/image';
 import api from '@libs/api.js';
 import styles from '@styles/Page.module.scss';
 import Button from '@components/button';
@@ -37,20 +38,47 @@ const Clientes: NextPage<Props> = ({ routes }) => {
                     <div
                         className={styles['featured-project']}
                         style={{
-                            backgroundImage:
-                                `url(${staticDataUrl}assets/img/backup-av-carillo-del-palau.webp)`,
+                            backgroundImage: `url(${staticDataUrl}assets/img/backup-av-carillo-del-palau.webp)`,
                             backgroundColor: '#5884b7',
                         }}
                     >
                         <div className={styles['logo-project']}>
-                            <img
-                                src='https://images.prismic.io/monogram/a769cac1-e373-4313-871a-f4f72dd9b7d8_m-logo.svg?auto=compress,format'
+                            <Image
+                                src={`${staticDataUrl}assets/img/logo-generalitat-de-catalunya-presidencia.svg`}
                                 alt=''
-                                width='232'
-                                height='167'
-                                className='flex-1'
-                                style={{ maxWidth: '250px', maxHeight: '125px' }}
+                                width='250'
+                                height='150'
                             />
+                        </div>
+                        <div className={styles['featured-project-card']}>
+                            <h2>Carrilló de la Presidència</h2>
+                            <hr />
+                            <p>
+                                Muntatge de la càmera i els micròfons per retransmetre, en directe i
+                                per streaming, la tocada del Carrilló del Palau de la Generalitat de
+                                Catalunya.
+                            </p>
+                        </div>
+                    </div>
+                    <div className={styles['project']}>
+                        <div className={styles['project-img']}>
+                            <div className={styles['bg-project-img']} style={{
+                            backgroundImage: `url(${staticDataUrl}assets/img/backup-av-santa-maria-de-pedralbes.webp)`,
+                        }}>
+                                <Image
+                                    src={`${staticDataUrl}assets/img/logo-santa-maria-de-pedralbes.svg`}
+                                    alt=''
+                                    loading='lazy'
+                                    width='250'
+                                    height='150'
+                                />
+                            </div>
+                        </div>
+                        <div className={styles['project-description']}>
+                            <h2 style={{ maxWidth: '260px' }}>Reial Monestir de Santa Maria de Pedralbes</h2>
+                            <p style={{ maxWidth: '290px' }}>
+                            Digitalització dels equips audiovisuals de la Sala d’Actes del Reial Monestir de Santa Maria de Pedralbes.
+                            </p>
                         </div>
                     </div>
                 </div>
