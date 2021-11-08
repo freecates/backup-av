ed1f1f0f292a5788a4942fb5f517e32a3e4095b0
Ramon Gil <ramon.gil@deporvillage.com>
2021-11-08 23:44:08 +0100

    main , build excludes to aviod sw error

--- next.config.js
+++ next.config.js
@@ -17,6 +17,7 @@ module.exports = withPWA({
       dest: 'public',
       runtimeCaching,
       disable: process.env.NODE_ENV === 'development',
+      buildExcludes: [/middleware-manifest.json$/]
   },
   reactStrictMode: true,
 })
